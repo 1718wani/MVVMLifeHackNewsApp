86b5b658d5422884293b1717e4707bd67c910289
im1293 <ikuya1293@gmail.com>
2021-01-17 08:31:28 +0900

    Add RetrofitをMoshiに変換した

--- app/src/main/java/com/androiddevs/mvvmnewsapp/api/RetrofitInstance.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/api/RetrofitInstance.kt
@@ -4,8 +4,9 @@ import com.androiddevs.mvvmnewsapp.util.Constants.Companion.BASE_URL
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 
+//Userserviceはインターフェースのためビルダーを使ってインスタンス化している
 class RetrofitInstance {
     companion object{
         private val retrofit by lazy{
@@ -16,7 +17,7 @@ class RetrofitInstance {
                 .build()
             Retrofit.Builder()
                 .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(MoshiConverterFactory.create())
                 .client(client)
                 .build()
         }
