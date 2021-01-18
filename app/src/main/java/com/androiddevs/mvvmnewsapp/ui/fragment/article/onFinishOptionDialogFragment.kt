94c3c706e568286324fa9a3c7525a655f34cc145
im1293 <ikuya1293@gmail.com>
2021-01-18 09:21:24 +0900

    Add DialogFragmentを実装してキャンセルを押すことで元画面まで戻るところまで実装済み

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragment/article/onFinishOptionDialogFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragment/article/onFinishOptionDialogFragment.kt
@@ -6,17 +6,24 @@ import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewTreeObserver
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
 import com.androiddevs.mvvmnewsapp.R
 
 class onFinishOptionDialogFragment : DialogFragment() {
 
+    private lateinit var listener: onFinishOptionDialogLister
+
     public interface onFinishOptionDialogLister {
-        public fun onDialogPositiveClick(dialog:DialogFragment)
-        public fun onDialogNegativeClick(dialog:DialogFragment)
+        public fun onDialogTweetClick(dialog:DialogFragment,which:Int)
+        public fun onDialogMemoClick(dialog:DialogFragment,which:Int)
+        public fun onDialogQuitClick(dialog:DialogFragment,which:Int)
     }
 
-//    var mLister:onFinishOptionDialogLister? = null
+    var mLister:onFinishOptionDialogLister? = null
 //
 //    override fun onAttach(context: Context?) {
 //        super.onAttach()
@@ -31,13 +38,23 @@ class onFinishOptionDialogFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
-            builder.setTitle(R.string.pick_on_finish_option)
-                    .setItems(R.array.arrayOnFinishOption,
-                        DialogInterface.OnClickListener {dialog, which ->
-                            val opitons = resources.getStringArray(R.array.arrayOnFinishOption)
-                            println(opitons[which])
-                        })
-            //これの意味は正確にはわかっていない。
+            builder.setTitle(R.string.title)
+                .setMessage(R.string.message)
+                    .setPositiveButton(R.string.tweet) {dialog, id ->
+                        Toast.makeText(activity, "Tweetします", Toast.LENGTH_SHORT).show()
+//                        mListener?.onDialogTweetClick(activity)
+                        }
+                    .setNegativeButton(R.string.memo) {dialog, id ->
+                        Toast.makeText(activity, "メモするよ", Toast.LENGTH_SHORT).show()
+                         }
+                    .setNeutralButton(R.string.quit) { dialog, id ->
+                    // Neutralボタンがタップされたときに実行される処理
+                        Toast.makeText(activity, "「無回答」がタップされた", Toast.LENGTH_SHORT).show()
+                        //popBackStackとはなんぞや
+                        findNavController().popBackStack(R.id.breakingNewsFragment,false)
+                     }
+
+            //キャンセル可能にする
             isCancelable = true
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
